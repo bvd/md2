5ab4a22202b5d67d114baf76fd04a30f9913575b
E.K. van Dalen <ek.vandalen@gmail.com>
2012-11-01 21:16:02 +0100

    packaging script missing arg

--- packmd2.sh
+++ packmd2.sh
@@ -1,1 +1,9 @@
-tar zcvf $1.tar.gz data log priv_cache priv_data data/md2/data/user data/md2/data/swf data/md2/data/css data/md2/data/js/tinymce log priv_data apps/md2/config/config.php apps/md2/config/database.php .htaccess
+#!/bin/bash
+if [ $# -lt 1 ]; then
+  echo 1>&2 "$0: not enough arguments"
+  exit 2
+elif [ $# -gt 1 ]; then
+  echo 1>&2 "$0: too many arguments"
+  exit 3
+fi
+tar zcvf $1.tar.gz log priv_cache priv_data data/md2/data/user data/md2/data/swf data/md2/data/css data/md2/data/js/tinymce log apps/md2/config/config.php apps/md2/config/database.php .htaccess
