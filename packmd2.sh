2829e0bb74c259fea7ae793ab0443f54390cb30e
E.K. van Dalen <ek.vandalen@gmail.com>
2012-11-01 21:21:17 +0100

    added googledb, crossdomain and favicon to packaging script

--- packmd2.sh
+++ packmd2.sh
@@ -6,4 +6,4 @@ elif [ $# -gt 1 ]; then
   echo 1>&2 "$0: too many arguments"
   exit 3
 fi
-tar zcvf $1.tar.gz log priv_cache priv_data data/md2/data/user data/md2/data/swf data/md2/data/css data/md2/data/js/tinymce log apps/md2/config/config.php apps/md2/config/database.php .htaccess
+tar zcvf $1.tar.gz log priv_cache priv_data data/md2/data/user data/md2/data/swf data/md2/data/css data/md2/data/js/tinymce log apps/md2/config/config.php apps/md2/config/database.php .htaccess crossdomain.xml google*.html favicon.ico
